98588d608b5e6bfdb729f6b984fce555180660d9
vanovvvvv <i.s.iwanow@mail.ru>
2024-05-31 11:55:08 +0400

    Изменил структуру

--- Homework/src/main/java/org/example/Task2.java
+++ Homework/src/main/java/org/example/Task2.java
@@ -10,9 +10,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-/*P.S. Насколько я понял из ТЗ, построчный вывод текста из файла должен быть аналогичным 1 заданию,
-т.е через каждые 2с по строчке в сопровождении даты и "- Отправляю сообщение:"*/
-//2** ОСТАНОВКА ВРУЧНУЮ В ИДЕЕ
 public class Task2 {
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -30,7 +27,7 @@ public class Task2 {
             Destination destination = session.createDestination("barbecue");
             // Создаю продюсера для отправки сообщений в очередь
             Producer producer = session.createProducer(destination);
-            // Отправляю строки из файла в бесконечную очередь, используя рекурсивный метод
+            // Отправляю строки из файла в бесконечную очередь
             resetToBeginning(filePath, producer);
             // Это не пригодиться в данной задаче, но все же путсь будет закрытие)
             session.close();
@@ -41,18 +38,19 @@ public class Task2 {
     }
 
     private static void resetToBeginning(String filePath, Producer producer) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Thread.sleep(2000);
-                producer.send(line);
+        while (true) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Thread.sleep(2000);
+                    producer.send(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+                break; // Выйти из бесконечного цикла, если файл не найден
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-            // Возвращаюсь на первую строчку, используя рекурсию
-            resetToBeginning(filePath, producer);
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
         }
     }
 }
@@ -71,8 +69,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-P.S. Насколько я понял из ТЗ, построчный вывод текста из файла должен быть аналогичным 1 заданию,
-т.е через каждые 2с по строчке в сопровождении даты и "- Отправляю сообщение:"
+//P.S. Насколько я понял из ТЗ, построчный вывод текста из файла должен быть аналогичным 1 заданию,
+//т.е через каждые 2с по строчке в сопровождении даты и "- Отправляю сообщение:"
 public class Task2 {
     public static void main(String[] args) {
         if (args.length != 1) {
